6ff0761bc4a03d83023fce1a9e8a654b7d132222
Sidmar Theodoro <theodorosidmar@gmail.com>
2020-02-03 19:02:18 -0300

    feat(config): Creates application bootstrap

---
+++ configuration-server/src/test/kotlin/com/theodorosidmar/configurationserver/ConfigurationServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.theodorosidmar.configurationserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
